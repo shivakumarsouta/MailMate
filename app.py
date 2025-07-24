366c4f6586c9c22012b0ab18ff678f8bb1eb0515
Shiva Kumar Souta <shivakumarsouta18@gmail.com>
2025-07-24 21:37:32 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from agents.email_agent import generate_email_response
+from utils.email_sender import send_email
+
+st.set_page_config(page_title="Auto Email Responder", layout="wide")
+st.title("MailMate - Think Less, Send Smart")
+
+email_text = st.text_area("Paste the email content you have recieved:", height=300)
+recipient_email = st.text_input("Recipient Email address")
+tone = st.selectbox("Select response tone", ["Professional", "Friendly", "Apologetic", "Persuasive"])
+
+if st.button("Generate & Send Email"):
+    if not recipient_email:
+        st.warning("Please enter the recipient Email address.")
+
+    else:
+        with st.spinner("Generating & Sending Email..."):
+            response = generate_email_response(email_text, tone)
+            send_status = send_email(recipient_email, response)
+            st.subheader("✉️ Response")
+            st.markdown(response, unsafe_allow_html=True)
+            if send_status:
+                st.success("Email sent successfully to {recipient_email}.")
+            else:
+                st.error("Failed to send Email.")
